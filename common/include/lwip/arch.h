148391961ee93aedad718df739596af25b35f7d3
oobles <oobles@fd1d1198-7684-4768-b9be-5b3c44f97897>
2003-04-17 00:21:00 +0000

    oops..  s32_t should be signed.

--- common/include/lwip/arch.h
+++ common/include/lwip/arch.h
@@ -47,7 +47,7 @@ typedef signed   char s8_t;
 typedef unsigned short u16_t;
 typedef signed   short s16_t;
 typedef unsigned long u32_t;
-typedef unsigned long s32_t;
+typedef signed   long s32_t;
 
 typedef u32_t mem_ptr_t;
 
