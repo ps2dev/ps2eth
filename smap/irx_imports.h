09544300295c192f3952272aec7294eb61b7b6b0
sp193 <pirorin187@gmail.com>
2017-02-26 19:36:12 +0800

    Removed unused imports from SMAP.

--- smap/irx_imports.h
+++ smap/irx_imports.h
@@ -4,18 +4,13 @@
 #include <irx.h>
 
 /* Please keep these in alphabetical order!  */
-#include <atad.h>
 #include <dev9.h>
 #include <intrman.h>
-#include <iomanX.h>
 #include <loadcore.h>
 #include <ps2ip.h>
-#include <sifcmd.h>
-#include <sifman.h>
 #include <stdio.h>
 #include <sysclib.h>
 #include <thbase.h>
-#include <thsemap.h>
 #include <thevent.h>
 
 #endif /* IOP_IRX_IMPORTS_H */
